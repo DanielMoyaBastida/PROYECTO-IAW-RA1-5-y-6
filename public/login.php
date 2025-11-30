7262e77bfc2d779f14951e4e2dace4e8bd34e9e9
AlejandroGarciaGar <1914265@alu.murciaeduca.es>
2025-11-30 13:46:20 +0100

    login 2

--- public/login.php
+++ public/login.php
@@ -1,7 +1,7 @@
 <?php
-// public/login.php
-require_once DIR . '/../src/functions.php';
+require_once __DIR__ . '/../src/functions.php';
 $pdo = getPDO();
+
 // verifica la validez del tokemm csrf
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!verify_csrf_token($_POST['csrf_token'] ?? null)) {
@@ -9,21 +9,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header('Location: ' . BASE_URL . 'login.php');
         exit;
     }
-// maneja el inicio de sesión del usuario, con diferentes validaciones, 
-// como que no estén vacíos y comprueba la base de datos
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // maneja el inicio de sesión del usuario, con diferentes validaciones, 
+    // como que no estén vacíos y comprueba la base de datos
     $username = trim($_POST['username'] ?? '');
     $password = $_POST['password'] ?? '';
 
     if ($username === '' || $password === '') {
         set_flash('error','Usuario y contraseña son obligatorios.');
-        header('Location: ' . 'https://github.com/DanielMoyaBastida/PROYECTO-IAW-RA1-5-y-6/blob/main/public/login.php' . 'login.php');
+        header('Location: ' . BASE_URL . 'login.php');
         exit;
     }
 
     $stmt = $pdo->prepare("SELECT id, username, password_hash FROM users WHERE username = ?");
     $stmt->execute([$username]);
     $user = $stmt->fetch();
+
     if ($user && password_verify($password, $user['password_hash'])) {
         session_regenerate_id(true);
         $_SESSION['user_id'] = $user['id'];
@@ -36,14 +37,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
 }
+
 // genera el formulario de inicio de sesión
-require_once DIR . '/../templates/header.php';
+require_once __DIR__ . '/../templates/header.php';
 ?>
 <h2>Login</h2>
 <form method="post" action="">
+  <?= csrf_field() ?>
   <label>Usuario<br><input type="text" name="username" value="<?= e($_POST['username'] ?? '') ?>"></label><br>
   <label>Contraseña<br><input type="password" name="password"></label><br><br>
   <button type="submit">Entrar</button>
 </form>
-<?php require_once DIR . '/../templates/footer.php'; 
+<?php require_once __DIR__ . '/../templates/footer.php'; 
 ?>
