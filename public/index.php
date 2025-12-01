7faf23d39f5ad43fd3104d3af3229950a1367c5b
DanielMoyaBastida <danimoyabas@gmail.com>
2025-12-01 17:39:28 +0100

    botón

--- public/index.php
+++ public/index.php
@@ -3,3 +3,15 @@
 header('Location: ../tickets/list.php');
 exit;
 ?>
+<html>
+<head>
+</head>
+<body>
+    <h1>Pulsa el enlace para ir a lista</h1>
+    <button>
+        <a href="tickets/list.php">
+        Ir a list
+        </a>
+    </button>
+</body>
+</html>
